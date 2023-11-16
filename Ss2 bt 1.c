269bfe72ca94bcbd8620af47be73f835afda8a4a
dvuday <147390370+dvuday@users.noreply.github.com>
2023-11-16 12:46:25 +0700

    Add files via upload

---
+++ Ss2 bt 1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int canh;
+	int chieuDai;
+	int chieuRong;
+	int banKinh;
+	float PI;
+	PI = 3.14;
+	int cVuong = canh*4;
+	int sVuong = canh*canh;
+	int cHCN = (chieuDai+chieuRong)*2;
+	int sHCN = chieuDai*chieuRong;
+	float cHinhTron = banKinh*PI;
+	float shinhTron = banKinh*banKinh*PI;
+}
---
+++ Ss2 bth 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int age;
+	bool sex;
+	float mark;
+	age = 18;
+	sex = 1;
+	mark = 9.5;
+	char studentid[] = {'S','v','0','0','1'};
+	char studentName[] = "Pham Van Minh";
+	double number1 =6.5 , number2 =8.2 , number3 =-7.4;
+	printf("Tuoi%d",age );
+	}
